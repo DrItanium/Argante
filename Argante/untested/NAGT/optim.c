66e111078b370ce7f8537a1516c9139dcf9f4d0d
shykta <>
2001-04-20 05:52:30 +0000

    Lots of bugfixes.
    Can now produce a 'clean' output image.

--- Argante/untested/NAGT/optim.c
+++ Argante/untested/NAGT/optim.c
@@ -37,6 +37,7 @@ struct sdes sys[]={
 
 struct bformat header;
 unsigned char *bytecode, *data;
+unsigned char *bcode_tmp;
 
 typedef struct _dpoint dpoint;
 struct _dpoint {
@@ -97,9 +98,9 @@ int follow(int loc)
 		return a1;
 	}
 
-	op=bytecode[loc * 12];
-	a1=bytecode[loc * 12 + 4];
-	bytecode[loc * 12] = CMD_HALT; /* Prevent loops, erase everything :) */
+	op=bcode_tmp[loc * 12];
+	a1=*((int *) &bytecode[loc * 12 + 4]);
+	bcode_tmp[loc * 12] = CMD_HALT; /* Prevent loops, erase everything :) */
 	
 	switch(op) {
 	case CMD_RET:
@@ -108,8 +109,6 @@ int follow(int loc)
 	case CMD_JMP:
 		return a1;
 	case CMD_CALL:
-		push_on_dpoint(a1);
-		break;
 	case CMD_ONFAIL: /* Almost forgot this! */
 		push_on_dpoint(a1);
 		break;
@@ -123,28 +122,135 @@ int follow(int loc)
 	return loc + 1;
 }
 
+/* A shabby code relocator/shortener.
+ * This is for writing out images.
+ *
+ * At first I thought this was easy... then I remembered
+ * we might call somewhere further on in the code...
+ */
+typedef struct _achain achain;
+struct _achain {
+	int target;
+	int loc;
+	achain *next, *prev;
+};
+
+achain *achead=NULL;
+
+void fixup(int inloc, int outloc)
+{
+	achain *a;
+	char op;
+	int *a1;
+
+	if (inloc<0 || inloc>header.bytesize)
+	{
+		fprintf(stderr, "Fell off the edge of the world during fixup!\n");
+		exit(-1);
+	}
+	if (outloc<0 || outloc>header.bytesize)
+	{
+		fprintf(stderr, "Jumped off the edge of the world during fixup!\n");
+		exit(-1);
+	}
+
+	/* Sort through chain looking for addresses we're at/have passed */
+	a=achead;
+	while (a)
+	{
+		if (a->target < inloc)
+		{
+			fprintf(stderr, "<!> Jump target lost (this is a DCA bug). Op %x, target %x, now at %x.\n", a->loc, a->target, inloc);
+			exit(-1);
+		}
+		if (a->target == inloc)
+		{
+			/* Ok, target found, adjust it */
+			*((int *)&bytecode[a->loc * 12 + 4])=outloc;
+//			fprintf(stderr, "<.> Repairing %x -> %x\n", a->loc, outloc); 
+			/* Throw it away now */
+			if (a->prev)
+				a->prev->next=a->next;
+			else
+				achead=a->next;
+			
+			if (a->next)
+				a->next->prev=a->prev;
+
+			free(a);
+		}
+
+		a=a->next;
+	}
+	
+	/* Don't put memcpy to the test */
+	if (inloc != outloc) memcpy(&bytecode[outloc * 12], &bytecode[inloc * 12], 12);
+	
+	op=bytecode[inloc * 12];
+	/* We have a rather large scratch space in bcode_tmp.
+	 * Seeing as we've already tested the inloc contents,
+	 * store the outloc over them */
+	a1=(int *) &bcode_tmp[inloc * 12];
+	*a1=outloc;
+	/* Get a pointer to a1 */
+	a1=(int *) &bytecode[outloc * 12 + 4];
+
+	switch(op) {
+	case CMD_JMP:
+	case CMD_CALL:
+	case CMD_ONFAIL:
+		/* If we can, correct the address at a1 */
+		if (*a1 <= inloc)
+		{
+			/* There has to be a better way than this, but I'm too dumb */
+			*a1=*((int *)&bcode_tmp[*a1 * 12]);
+		}
+		else /* or if not save it for later */
+		{
+		//	fprintf(stderr, "<.> Saving %x -> %x\n", inloc, *a1); 
+			a=malloc(sizeof(achain));
+			a->loc=outloc;
+			a->target=*a1;
+			a->next=achead;
+			a->prev=NULL;
+			achead=a;
+		}
+		break;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	int i;
-	int dbc;
-	int ldba;
-	FILE *in;
+	int dbc, ubc; /* DeadByteCount, UsedByteCount */
+	int ldba; /* LastDeadByteAddress */
+	FILE *in, *out;
 
 	printf("\nDead Code Analyser - (C) 2001 James Kehl <ecks@optusnet.com.au>\n");
 	printf("based on Disassembler - (C) 2000 Maurycy Prodeus <z33d@eth-security.net>\n");
 	printf("for %s ver %d.%03d (C) 2000 Michal Zalewski <lcamtuf@tpi.pl>\n\n",
               SYSNAME,SYS_MAJOR,SYS_MINOR);
 	
-	if (argc!=2){
-	    printf("Usage: %s <filename.img>\n",argv[0]);
+	if (argc!=2 && argc!=3){
+	    printf("Usage: %s <input.img> [output.img]\n",argv[0]);
 	    exit(-1);
 	}
-	in=fopen(argv[1],"r");
+
+	in=fopen(argv[1],"rb");
 	if (!in){
-	    perror("open");
+	    perror("open input file");
 	    exit(-1);
 	}	
+	if (argc==3)
+	{
+		out=fopen(argv[2],"wb");
+		if (!out){
+		    perror("open output file");
+		    exit(-1);
+		}
+	} else out=NULL;
 	
+	/* Simple tests to verify image... Endian Fixme! */
 	if(fread(&header,sizeof(header), 1, in)!=1){
 	    printf("%s is broken.\n",argv[1]);
 	    exit(-1);
@@ -153,8 +259,12 @@ int main(int argc, char **argv)
 	    printf("%s: invalid header.\n",argv[1]);
 	    exit(-1);
 	}
-	bytecode=(char*) malloc(header.bytesize*12);
-	if (!bytecode && header.bytesize>0){
+
+	/* Load 2 copies of bytecode. */
+	bytecode=malloc(header.bytesize*12);
+	bcode_tmp=malloc(header.bytesize*12);
+	
+	if ((!bytecode || !bcode_tmp) && header.bytesize>0){
 	    printf("Not enough memory!\n");
 	    exit(-1);
 	}
@@ -162,6 +272,10 @@ int main(int argc, char **argv)
 	    printf("I can't read %s.\n",argv[1]);
 	    exit(-1);
 	}
+
+	memcpy(bcode_tmp, bytecode, header.bytesize * 12);
+
+	/* Load data */
 	data=(char*) malloc(header.datasize*4);
 	if (!data && header.datasize>0){
 	    printf("Not enough memory!\n");
@@ -171,22 +285,24 @@ int main(int argc, char **argv)
 	    printf("I can't read %s.\n",argv[1]);
 	    exit(-1);
 	}
+	/* We've finished with the input image */
 	fclose(in);
 
 	i=header.init_IP;
 	while ((i=follow(i))>=0); /* Yer classic recursion loop, mate! */
 
 	ldba=-1;
-	dbc=0;
+	dbc=ubc=0;
 	
 	/* Ok, check for leftovers */
 	for(i=0;i<header.bytesize;i++)
 	{
-		if (bytecode[i * 12] != CMD_HALT)
+		if (bcode_tmp[i * 12] != CMD_HALT)
 		{
 			dbc++;
 			if (ldba < 0) ldba=i;
 		} else {
+			ubc++;
 			if (0 < ldba)
 			{
 				fprintf(stderr, "<-> bytecode packets %x-%x are unused\n", ldba, i);
@@ -194,9 +310,47 @@ int main(int argc, char **argv)
 			}
 		}
 	}
-	fprintf(stderr, "<+> Total %d unused bytecode packets\n", dbc);
+	/* In case I stuff up */
+	if (ubc != header.bytesize - dbc)
+		fprintf(stderr, "<!> Mismatch! Expected used bytes: %d\n", header.bytesize - dbc);
+
+	fprintf(stderr, "<+> Bytecode packets: %d unused, %d used\n", dbc, ubc);
 	
+	/* Now translate code if we have an output file */
+	if (out)
+	{
+		i=ubc;
+		/* Now UBC and DBC become indexes. Sorry */
+		ubc=dbc=0;
+		
+		for(dbc=0;dbc<header.bytesize;dbc++)
+		{
+			if (bcode_tmp[dbc * 12] == CMD_HALT)
+			{
+				fixup(dbc, ubc);
+				ubc++;
+			}
+		}
+	
+		/* Output stage */
+		header.bytesize=i;
+		
+		if(fwrite(&header, sizeof(header), 1, out)!=1){
+			perror("fwrite");
+			exit(-1);
+		}
+		if(fwrite(bytecode, header.bytesize * 12, 1, out)!=1){
+			perror("fwrite");
+			exit(-1);
+		}
+		if(fwrite(data, header.datasize * 4, 1, out)!=1){
+			perror("fwrite");
+			exit(-1);
+		}
+		fclose(out);
+	}
 	free(data);
 	free(bytecode);
+	free(bcode_tmp);
 	return 0;
 }
